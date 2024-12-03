cc71870796be5593999de2b0d0ed740098ff6fc8
Vlad-Ribeiro <vlad.ribeiro@sptech.school>
2024-12-03 18:02:00 -0300

    Dash com kpis responsivas e estilizada

--- site/js/sessao.js
+++ site/js/sessao.js
@@ -3,11 +3,14 @@ function validarSessao() {
   var email = sessionStorage.EMAIL_USUARIO;
   var nome = sessionStorage.NOME_USUARIO;
   var fkEmpresa = sessionStorage.FK_EMPRESA;
+  var empNome = sessionStorage.EMP_NOME; 
 
   var b_usuario = document.getElementById("b_usuario");
+  var b_empresa = document.getElementById('b_empresa')
 
   if (email != null && nome != null) {
     b_usuario.innerHTML = nome;
+    b_empresa.innerHTML = empNome;
   } else {
     window.location = "../pages/login.html";
   }
@@ -15,7 +18,7 @@ function validarSessao() {
 
 function limparSessao() {
   sessionStorage.clear();
-  window.location = "../login.html";
+  window.location = "./login.html";
 }
 
 // carregamento (loading)
