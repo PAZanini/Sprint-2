d2f8db3eca34bc6148c96eb04acbada1a97a72df
Vlad-Ribeiro <vlad.ribeiro@sptech.school>
2024-12-10 13:45:00 -0300

    Modificações no `pages`, `app.js`, novas rotas criadas e nova dashboard

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -18,11 +18,12 @@ function autenticar(req, res) {
         if (resultadoAutenticar.length == 1) {
           console.log(resultadoAutenticar);
           res.json({
-            idUSER: resultadoAutenticar[0].idUSER,
+            idUSER: resultadoAutenticar[0].id,
             email: resultadoAutenticar[0].email,
             nome: resultadoAutenticar[0].nome,
             senha: resultadoAutenticar[0].senha,
             empNome: resultadoAutenticar[0].empNome,
+            fkEmpresa: resultadoAutenticar[0].empresaId,
           });
         } else if (resultadoAutenticar.length == 0) {
           res.status(403).send("Email e/ou senha inválido(s)");
--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -7,7 +7,7 @@ function autenticar(email, senha) {
     senha
   );
   var instrucaoSql = `
-        SELECT nomeCompleto as nome, email, senha, fkEmpresa as empresaId, nome as empNome FROM usuario join empresa on idEmpresa = fkEmpresa WHERE email = '${email}' AND senha = '${senha}';
+        SELECT idUSER AS id, nomeCompleto as nome, email, senha, fkEmpresa AS empresaId, nome as empNome FROM usuario join empresa on idEmpresa = fkEmpresa WHERE email = '${email}' AND senha = '${senha}';
     `;
   console.log("Executando a instrução SQL: \n" + instrucaoSql);
   return database.executar(instrucaoSql);
